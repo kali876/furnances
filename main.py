7c0a1f7515f6a14d4598cabbd6860ed8ddb949d2
PaverNet <pawel@cin.pl>
2023-10-09 18:57:10 +0200

    update

--- main.py
+++ main.py
@@ -823,7 +823,7 @@ class BakingProcess:
         currentStep = self.getCurrentStep()
         currentStepStartTime = self.getStartTime()
 
-        for seq in range(currentStep.getStepNumber()):
+        for seq in range(1, currentStep.getStepNumber()):
             step = self.getStepByNumber(seq)
             currentStepStartTime = currentStepStartTime + step.getDuration()
 
