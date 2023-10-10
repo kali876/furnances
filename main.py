7443a9dc9026ce4a0fbce1f4688d877e8552ef1f
PaverNet <pawel@cin.pl>
2023-10-10 09:54:51 +0200

    update

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
+from email import encoders
 
 os.chdir("/root/furnances")
 
