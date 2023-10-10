ada0c2c0435ed7c85e77c19b26dea75220bde6eb
PaverNet <pawel@cin.pl>
2023-10-10 16:04:25 +0200

    update

--- main.py
+++ main.py
@@ -1077,7 +1077,7 @@ def main():
             process.getFurnance().heateron(5)
         elif differenceTemperature <= -0.2 and differenceTemperature > -0.7 and currentTrend == 1:
             process.getFurnance().heateron(15)
-        elif differenceTemperature <= -0.8 and differenceTemperature > -2 and currentTrend == 1:
+        elif differenceTemperature <= -0.7 and differenceTemperature > -2 and currentTrend == 1:
             process.getFurnance().heateron(20)
         elif differenceTemperature <= -2 and differenceTemperature > -3.5 and currentTrend == 1:
             process.getFurnance().heateron(30)
@@ -1085,7 +1085,7 @@ def main():
             process.getFurnance().heateron(40)
         elif differenceTemperature <= -5 and differenceTemperature > -100 and currentTrend == 1:
             process.getFurnance().heateron(60)
-        if differenceTemperature != 0 and differenceTemperature > -0.1 and currentTrend == 0:
+        if differenceTemperature <= 0 and differenceTemperature > -0.1 and currentTrend == 0:
             process.getFurnance().heateron(5)
         elif differenceTemperature <= -0.1 and differenceTemperature > -1 and currentTrend == 0:
             process.getFurnance().heateron(15)
@@ -1095,7 +1095,7 @@ def main():
             process.getFurnance().heateron(30)
         elif differenceTemperature <= -5 and differenceTemperature > -100 and currentTrend == 0:
             process.getFurnance().heateron(40)
-        if differenceTemperature < 1 and differenceTemperature > -0.3 and currentTrend == 2:
+        if differenceTemperature <= 1 and differenceTemperature > -0.3 and currentTrend == 2:
             process.getFurnance().heateron(5)
         elif differenceTemperature < -0.3 and differenceTemperature > -1.5 and currentTrend == 2:
             process.getFurnance().heateron(15)
