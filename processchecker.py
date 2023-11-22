ed91bdc2238daf8144b3382d8a2fcd064e3cd570
PaverNet <pawel@cin.pl>
2023-11-22 21:19:14 +0100

    update

--- processchecker.py
+++ processchecker.py
@@ -182,18 +182,18 @@ def pushnotifi(message):
         verify=False,
         timeout=10,
     )
-def setFurnanceFans(id):
-    furnance=Furnance(id)
-    curent_temp = furnance.getTemperature()
-    if curent_temp > 80:
-        furnance.exhaustValveOpen()
-        furnance.freshairValveOpen()
-        furnance.cyrcfanon()
-        furnance.exhaustfanon()
-    else:
-        furnance.heateroff()
-        furnance.cyrcfanoff()
-        furnance.exhaustfanoff()
+#def setFurnanceFans(id):
+#    furnance=Furnance(id)  #sprawdzić czy zmiana nazwy zmiennej wyeliminuje dublowanie się rządań do pieca.
+#    curent_temp = furnance.getTemperature()
+#    if curent_temp > 80:
+#        furnance.exhaustValveOpen()
+#        furnance.freshairValveOpen()
+#        furnance.cyrcfanon()
+#        furnance.exhaustfanon()
+#    else:
+ #       furnance.heateroff()
+ #       furnance.cyrcfanoff()
+ #       furnance.exhaustfanoff()
 
 
 def processchecker():
@@ -203,7 +203,7 @@ def processchecker():
         furnance=Furnances(file)
         process_already_exist = furnance.isProcessExist()
         if process_already_exist == False:
-            setFurnanceFans(furnance.getFurnance())
+            #setFurnanceFans(furnance.getFurnance())
             checked_cycle = furnance.getCheckedCycle()
             proces_start = furnance.getProcessStart()
             if proces_start == True and checked_cycle != None:
